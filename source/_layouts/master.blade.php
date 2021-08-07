adc8e48f7ed85a3732e7d64141589b4fcdba5579
yj7o5 <yawarjamal@gmail.com>
2021-08-07 15:08:56 -0500

    Remove template owner detail and fix link

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -40,8 +40,7 @@
             <small>
                 &copy; <span data-year></span> {{ $page->owner->name }} &nbsp;&bull;&nbsp;
                 <a href="/feed.atom">RSS</a> &nbsp;&bull;&nbsp;
-                Maintained by Ranie Santos &nbsp;&bull;&nbsp;
-                <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>
+                <a href="https://github.com/{{$page->owner->github}}/blog">GitHub repo</a>
             </small>
         </footer>
     </section>
