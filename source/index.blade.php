55615d1d9f92216be51649774aa56c1907bc11c2
yj7o5 <yawarjamal@gmail.com>
2021-08-07 15:37:27 -0500

    Small update to home page text

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <h1>Yawar Jamal</h1>
 
-    <p>Houstonian, Coder, Programmer, Developer, or whatever you want to put in.</p>
+    <p>Houstonian, Coder, Programmer, Developer, Tinker, or anything in between.</p>
 
-    <p>I like surrounding myself deep in the oceans of bits and bytes and understanding the whys of many unknowns that are yet to discover by my single soul</p>
+    <p>I like surrounding myself deep into the oceans of bits and bytes while understanding the whys of many unknowns that are yet to discovered by my soul</p>
 
     <p>Check out some of my <a href="/posts">thoughts</a> that you might find intriguing.</p>
 @endsection
