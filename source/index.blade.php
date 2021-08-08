67f7e2ed8440e3f36440b0eed3f5f7799e347171
yj7o5 <yawarjamal@gmail.com>
2021-08-07 21:16:19 -0500

    Update main page

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,5 +7,7 @@
 
     <p>I like surrounding myself deep into the oceans of teeny-tiny bits while trying to understand the whys of many knowns and discovering the many unknowns, that which, are yet to be known.</p>
 
+    <p>Oh and I can't wait the first starship to land on the surface of Mars, go SpaceX</p>
+
     <p>Check out some of my <a href="/posts">thoughts</a> that you might find intriguing.</p>
 @endsection
